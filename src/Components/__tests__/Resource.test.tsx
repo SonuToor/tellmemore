c412bdb8915d39da1874d4e426382fba6671f93b
SonuToor <savrooptoor92@gmail.com>
2020-03-19 19:57:25 -0400

    added another test, still a wip as I want to test more in it

--- src/Components/__tests__/Resource.test.tsx
+++ src/Components/__tests__/Resource.test.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Resource, { ResourceDiv } from "../ResourceComponents/Resource";
 import { SelectedTrendContext } from "../../Context/SelectedTrendContext";
-import fetchResources from "../../Services/fetchResources";
+import fetcher from "../../Services/fetchResources";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../../theme";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, wait } from "@testing-library/react";
 
 test("A resource will initially show a loading component when clicked", () => {
   let paramsReddit = ["Find", "Me", "Something"];
@@ -33,7 +33,7 @@ test("Clicking the Resource Div calls the onClick handler", () => {
     </ThemeProvider>
   );
 
-  const { getByTestId, getByText } = render(tree);
+  const {} = render(tree);
 
   let section = document.getElementsByTagName("section");
   fireEvent.click(section[0]);
@@ -41,46 +41,31 @@ test("Clicking the Resource Div calls the onClick handler", () => {
   expect(onClick).toHaveBeenCalled();
 });
 
-test("A resource that has no results for a trend will display a message saying so", () => {
-  // TODO
-  // onClick the resource only renders the loading component
-  // it needs to mimic getResources and call fetchResources
-  // or at the very least just set the resources so renderResources can be fired
-
+test("Clicking a resource calls the fetchResources function", async () => {
   let paramsReddit = ["Find", "Me", "Something"];
-  let paramsWiki = ["Nothing", "To", "Find"];
 
-  // jest.mock("../../Services", () => ({ fetchResources: jest.fn() }));
-
-  let fetchResources = jest.fn().mockImplementation(() => {
-    return Promise.resolve().then(() => []);
-  });
-
-  let noResults = jest.fn(async () => {
-    // let results = await fetchResources;
-    // return Promise.resolve().then(() => []);
-  });
-
-  let results = jest.fn(() => [[]]);
+  let fetchResources = jest
+    .spyOn(fetcher, "fetchResources")
+    .mockImplementation(() => {
+      return Promise.resolve().then(() => {
+        return [[]];
+      });
+    });
 
   const tree = (
     <ThemeProvider theme={theme}>
-      <Resource resource={"Reddit"} params={paramsReddit} icon={null}>
-        <ResourceDiv onClick={noResults}></ResourceDiv>
-      </Resource>
-      <Resource resource={"Wiki"} params={paramsWiki} icon={null}>
-        <ResourceDiv onClick={noResults}></ResourceDiv>
-      </Resource>
+      <Resource resource={"Reddit"} params={paramsReddit} icon={null} />
     </ThemeProvider>
   );
 
-  const { container, getByText } = render(tree);
+  const { container, debug, getByText } = render(tree);
 
   let reddit = getByText("Reddit");
-  let wiki = container.lastChild;
-
   fireEvent.click(reddit);
 
-  console.log(document.body.innerHTML);
-  // expect(noResults).toHaveBeenCalled();
+  debug(container);
+
+  await wait();
+
+  expect(fetchResources).toHaveBeenCalled();
 });
--- src/Services/fetchResources.ts
+++ src/Services/fetchResources.ts
@@ -2,26 +2,51 @@ import fetchReddit from "./fetchReddit";
 import { fetchWiki } from "./fetchWiki";
 import { fetchWikiSecondary } from "./fetchWiki";
 
-const fetchResources = async (resource: string, params: string[]) => {
-  let paramsStringLength = params.length;
-  let paramsString = params.map((param, index) => {
-    if (index !== paramsStringLength - 1) {
-      return `${param}+`;
-    } else {
-      return `${param}`;
-    }
-  });
-  if (resource === "Reddit") {
-    return fetchReddit(paramsString);
-  } else if (resource === "Wiki") {
-    let results = await fetchWiki(paramsString);
-    // if querying Wikipedia with all words of the trend doesn't work, try each word invididually
-    if (results.length === 0) {
-      return await fetchWikiSecondary(paramsString);
-    } else {
-      return fetchWiki(paramsString);
+let fetcher = {
+  fetchResources: async (resource: string, params: string[]) => {
+    let paramsStringLength = params.length;
+    let paramsString = params.map((param, index) => {
+      if (index !== paramsStringLength - 1) {
+        return `${param}+`;
+      } else {
+        return `${param}`;
+      }
+    });
+    if (resource === "Reddit") {
+      return fetchReddit(paramsString);
+    } else if (resource === "Wiki") {
+      let results = await fetchWiki(paramsString);
+      // if querying Wikipedia with all words of the trend doesn't work, try each word invididually
+      if (results.length === 0) {
+        return await fetchWikiSecondary(paramsString);
+      } else {
+        return fetchWiki(paramsString);
+      }
     }
   }
 };
+// const fetchResources = async (resource: string, params: string[]) => {
+//   let paramsStringLength = params.length;
+//   let paramsString = params.map((param, index) => {
+//     if (index !== paramsStringLength - 1) {
+//       return `${param}+`;
+//     } else {
+//       return `${param}`;
+//     }
+//   });
+//   if (resource === "Reddit") {
+//     return fetchReddit(paramsString);
+//   } else if (resource === "Wiki") {
+//     let results = await fetchWiki(paramsString);
+//     // if querying Wikipedia with all words of the trend doesn't work, try each word invididually
+//     if (results.length === 0) {
+//       return await fetchWikiSecondary(paramsString);
+//     } else {
+//       return fetchWiki(paramsString);
+//     }
+//   }
+// };
 
-export default fetchResources;
+// export default fetchResources;
+
+export default fetcher;
